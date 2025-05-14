bf8d4c26fae3574cf4cf2f15d3fd857c2e644a81
Daria-Voloshanovska <voloshanovskadaria@gmail.com>
2025-05-14 09:58:20 +0200

    'lazy'

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Homework06 from './homeworks/Homework06/Homework06';
+// import Lesson06 from './lessons/Lesson06/Lesson06';
+
+
+
+function App() {
+
+  return (
+    <>
+    <Homework06/>
+      {/*<Lesson06/>*/}
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/SpaceMission.tsx
@@ -0,0 +1,29 @@
+export default function SpaceMission() {
+  const shipName: string = "Apollo 11";
+  let crewCount: number = 3;
+  let missionStarted: boolean = true;
+  const crewNames: string[] = ["Нил Армстронг", "Майкл Коллинз", "Базз Олдрин"];
+  const captain: [string, number] = ["Нил Армстронг", 38];
+
+  function startMission(shipName: string) {
+    return `Миссия ${shipName} началась!`;
+  }
+
+  return (
+    <>
+      <h2>Названием миссии: {shipName}</h2>
+      <p>Экипаж: {crewCount} человека</p>
+      <p>Члены экипажа:</p>
+      <ul>
+        {crewNames.map((crew) => (
+          <li>{crew === "Нил Армстронг" ? captain.join(", ") : crew}</li>
+        ))}
+      </ul>
+      <p>
+        {missionStarted
+          ? startMission(shipName)
+          : `Миссия ${shipName} ещё не началась!`}
+      </p>
+    </>
+  );
+}
